1f307c3397781633c0315d4994743eb28cd77b54
Andy Jones <andy@makeitsimple.info>
2026-03-16 18:53:59 +0000

    Tidied up

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,10 @@
 module.exports = function(eleventyConfig) {
-  // Passthrough copy - preserve all existing static assets
-  eleventyConfig.addPassthroughCopy({ "src/wp-content": "wp-content" });
-  eleventyConfig.addPassthroughCopy({ "src/wp-includes": "wp-includes" });
+  // Passthrough copy - static assets
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
   eleventyConfig.addPassthroughCopy({ "src/_headers": "_headers" });
   eleventyConfig.addPassthroughCopy({ "src/_redirects": "_redirects" });
-  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
   eleventyConfig.addPassthroughCopy({ "src/_routes.json": "_routes.json" });
 
   return {
